4c2cd8b5d64b4312bd85b5620f01e556f9edc1c6
Robin Verlangen <robin+github@us2.nl>
2016-06-10 13:04:56 +0200

    Fixing bug

---
+++ docker/run_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker run --name pubsubrest \
+	-v /etc/pubsub_kafa_rest.json:/etc/pubsub_kafa_rest.json \
+	-v /tmp/key.json:/tmp/key.json \
+	-d \
+	--restart=always \
+	--ulimit nofile=262144:262144 \
+	--memory="1G" \
+	pubsubrest
